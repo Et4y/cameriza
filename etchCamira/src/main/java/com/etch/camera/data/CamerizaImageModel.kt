086742f95d1cac0c58da585a6eaabff73aca8ad5
Abdallah Nader <pro_abdallah@hotmail.com>
2021-10-12 14:04:55 +0200

    1.6.5

--- etchCamira/src/main/java/com/etch/camera/data/CamerizaImageModel.kt
+++ etchCamira/src/main/java/com/etch/camera/data/CamerizaImageModel.kt
@@ -3,10 +3,8 @@ package com.etch.camera.data
 import android.net.Uri
 
 class CamerizaImageModel {
-
     var isSelected: Boolean = false
     var type: String? = null
     var image: String = ""
     var uri: Uri? = null
-
 }
